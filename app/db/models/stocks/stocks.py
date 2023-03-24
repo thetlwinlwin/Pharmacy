818965e18382b8d45de157085054c318ee8ca2dc
thetlwinlwin <minthantlwin8@gmail.com>
2023-03-25 00:44:16 +0630

    create stock table

---
+++ app/db/models/stocks/stocks.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.db.base import Base
+
+
+# TODO: should add reorder level.
+class Stock(Base):
+    """
+    To track stocks.
+    """
+
+    __tablename__ = "stocks"
+    id = Column(Integer, primary_key=True)
+    product_id = Column(
+        Integer,
+        ForeignKey("products.id", ondelete="CASCADE"),
+        index=True,
+    )
+    quantity_unit_id = Column(
+        Integer, ForeignKey("quantity_units.id", ondelete="SET NULL")
+    )
+    stock_quantity = Column(Integer, nullable=False)
+    quantity_unit = relationship(
+        "QuantityUnit",
+        uselist=False,
+    )
+    product = relationship(
+        "Products",
+        lazy=False,
+        uselist=False,
+    )
